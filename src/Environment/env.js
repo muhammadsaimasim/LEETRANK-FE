f54b64c74aab51f48924dd3552de65e3cab693f3
Saim123321 <saimasimkhan8@gmail.com>
2026-02-17 16:21:21 +0500

    validation fixes

--- src/Environment/env.js
+++ src/Environment/env.js
@@ -1,5 +1,5 @@
-export const BASE_URL = "http://localhost:5000/api";
-export const BASE_URL_IMG = "http://localhost:5000";
+// export const BASE_URL = "http://localhost:5000/api";
+// export const BASE_URL_IMG = "http://localhost:5000";
 
-// export const BASE_URL = "https://leetrank-api-gwfrc0c0azbwafdt.southeastasia-01.azurewebsites.net/api";
-// export const BASE_URL_IMG = "https://leetrank-api-gwfrc0c0azbwafdt.southeastasia-01.azurewebsites.net";
+export const BASE_URL = "https://leetrank-api-gwfrc0c0azbwafdt.southeastasia-01.azurewebsites.net/api";
+export const BASE_URL_IMG = "https://leetrank-api-gwfrc0c0azbwafdt.southeastasia-01.azurewebsites.net";

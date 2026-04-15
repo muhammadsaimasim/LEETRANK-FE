2619a98ac7131ec4274f2b988cfeef2a76730b06
Saim123321 <saimasimkhan8@gmail.com>
2026-04-15 21:34:08 +0500

    fixes

--- src/Environment/env.js
+++ src/Environment/env.js
@@ -1,8 +1,8 @@
-export const BASE_URL = "http://localhost:5000/api";
-export const BASE_URL_IMG = "http://localhost:5000";
+// export const BASE_URL = "http://localhost:5000/api";
+// export const BASE_URL_IMG = "http://localhost:5000";
 
-// export const BASE_URL = "https://leetrank-be.vercel.app/api";
-// export const BASE_URL_IMG = "https://leetrank-be.vercel.app/";
+export const BASE_URL = "https://leetrank-be.vercel.app/api";
+export const BASE_URL_IMG = "https://leetrank-be.vercel.app/";
 
 // export const BASE_URL = "https://leetrank-api-gwfrc0c0azbwafdt.southeastasia-01.azurewebsites.net/api";
 // export const BASE_URL_IMG = "https://leetrank-api-gwfrc0c0azbwafdt.southeastasia-01.azurewebsites.net";
